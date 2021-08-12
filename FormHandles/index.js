0e025f38ad143f9cea4fd0df4eddf8174094fab5
Giovanni Gomez R <giovannyg32@gmail.com>
2021-08-11 20:11:32 -0500

    Se modifican metodos de trabajo con tipo de datos check, rb o selects

--- FormHandles/index.js
+++ FormHandles/index.js
@@ -1,4 +1,27 @@
+const formData = document.querySelector("form");
 
-const inputNombre = document.getElementById('input-nombre');
+formData.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Enviado ... ");
+  const inputNombre = document.getElementById("input-nombre");
+  console.log(inputNombre.value);
 
-console.log(nombre.value);
+
+  //Radio buttons
+  const checked = document.querySelector('input[name="genero"]:checked');
+  if(checked) console.log(checked.value);
+
+  //Checkbox
+  const check = document.getElementById('option-1');
+
+  console.log(check.checked);
+
+
+  //Select
+
+  const selectOptions = document.getElementById('select-options')
+  console.log(selectOptions.value);
+
+});
+
+
